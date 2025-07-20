050f64f95e3b10055aa9fa3af810f43817f91a63
Marcelocarrascoduoc <marce.carrascogduocuc.cl>
2025-07-20 00:42:13 -0400

    Subida de nuevo proyecto (Milo-store5) al repositorio Milo-store3

--- src/app/pipes/formateo-fecha.pipe.ts
+++ src/app/pipes/formateo-fecha.pipe.ts
@@ -5,12 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true
 })
 export class FormateoFechaPipe implements PipeTransform {
-
-transform(value: string): string {
-    const fecha = new Date(value);
-    const dia = String(fecha.getDate()).padStart(2, '0');
-    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
-    const año = fecha.getFullYear();
-    return `${dia}/${mes}/${año}`;  // DD/MM/AAAA
+  transform(value: string): string {
+    // Asegura que solo se tome la parte de fecha, sin hora
+    const fecha = value.split('T')[0]; // '2025-07-18T00:00:00.000Z' → '2025-07-18'
+    const [año, mes, dia] = fecha.split('-');
+    return `${dia}/${mes}/${año}`; // dd/mm/yyyy
   }
 }
